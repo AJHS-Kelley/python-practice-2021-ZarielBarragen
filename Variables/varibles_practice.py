d0bc2cd165a93948d63f3a2f6953b4db33266188
Isaiah R <90346468+ZarielBarragen@users.noreply.github.com>
2021-10-29 14:39:40 -0400

    4A V0.1
    Added 3 common types of data, including an Integer, Floating point, and a String

--- Variables/varibles_practice.py
+++ Variables/varibles_practice.py
@@ -1,1 +1,8 @@
- # Variables Practice, Isaiah Reyes, 10/19/21 2:23pm,  version 0.0 
+ # Variables Practice, Isaiah Reyes, 10/19/21 2:38pm,  version 0.1 
+
+ # Three CommonTypes of Data
+
+   lives_remaining = 10 #INTERGERS, any +/- WHOLE number and 0
+   forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal.
+   death_cry = "Cower in awe! Recoil in despair! Cry your heart out!! Because my art...IS AN EXPLOSION!!"
+   # Line 7 is a STRING -- collection of letters/numbers/other chars...
